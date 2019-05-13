e22c4e4510fa1966e965d992b796f82e63c055bb
huylam98it <huylam98it@gmail.com>
2019-05-14 03:00:17 +0700

    aada

--- src/main/java/com/huylam98it/spboot/controller/HomeController.java
+++ src/main/java/com/huylam98it/spboot/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.huylam98it.spboot.controller;
 
+import java.io.File;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,6 +10,13 @@ public class HomeController {
 
 	@GetMapping(value = "/")
 	public String homePage() {
+		File file = new File("C:/images");
+		try {
+			if (file.exists() == false)
+				file.mkdirs();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
 		return "home";
 	}
 
